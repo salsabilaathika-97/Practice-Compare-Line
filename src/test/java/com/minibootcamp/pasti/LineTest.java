7aa8f282e0943696a4182a09249cbbeff4a447e7
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-23 22:14:42 +0800

    feat: add test case to return value more than zero if at least one line point is different

--- src/test/java/com/minibootcamp/pasti/LineTest.java
+++ src/test/java/com/minibootcamp/pasti/LineTest.java
@@ -1,6 +1,8 @@
 package com.minibootcamp.pasti;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,13 @@ public class LineTest {
 
     @Test
     void getLength_ShouldReturnZero_WhenAllInputAreSame() {
-        Line line = new Line(1,1,1,1);
+        Line line = new Line(1, 1, 1, 1);
         assertEquals(0, line.getLength(), 0);
+    }
+
+    @Test
+    void getLength_ShouldReturnValueMoreThanZero_WhenAtLeastOneInputIsDifferent() {
+        Line line = new Line(2, 1, 1, 1);
+        assertEquals(1, line.getLength(), 0);
     }
 }
