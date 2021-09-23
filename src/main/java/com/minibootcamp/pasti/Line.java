cb93392fa53ad8e7e9ac6b1d1b6854e56ff47372
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-23 21:55:48 +0800

    feat: create test to return length zero and add new empty constructor with getLength method

--- src/main/java/com/minibootcamp/pasti/Line.java
+++ src/main/java/com/minibootcamp/pasti/Line.java
@@ -1,5 +1,13 @@
 package com.minibootcamp.pasti;
 
 public class Line {
+    private double x1, y1, x2, y2;
 
+    public Line(double x1, double y1, double x2, double y2){
+
+    }
+
+    public double getLength(){
+        return 0;
+    }
 }
--- src/test/java/com/minibootcamp/pasti/LineTest.java
+++ src/test/java/com/minibootcamp/pasti/LineTest.java
@@ -1,5 +1,14 @@
 package com.minibootcamp.pasti;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class LineTest {
 
+    @Test
+    void getLength_ShouldReturnZero_WhenAllInputAreSame() {
+        Line line = new Line(1,1,1,1);
+        assertEquals(0, line.getLength(), 0);
+    }
 }
