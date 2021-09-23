5e9d5803b8e316370aaae599f305e018571a619a
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-23 21:57:36 +0800

    feat: modify constructor to store parameters

--- src/main/java/com/minibootcamp/pasti/Line.java
+++ src/main/java/com/minibootcamp/pasti/Line.java
@@ -1,13 +1,16 @@
 package com.minibootcamp.pasti;
 
 public class Line {
-    private double x1, y1, x2, y2;
+    private final double x1, y1, x2, y2;
 
-    public Line(double x1, double y1, double x2, double y2){
-
+    public Line(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
-    public double getLength(){
+    public double getLength() {
         return 0;
     }
 }
