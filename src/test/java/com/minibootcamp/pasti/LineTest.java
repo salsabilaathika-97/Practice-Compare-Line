771ec889dbb1713614e4c2fa1f696f0026849967
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-23 22:25:36 +0800

    feat: add test case to check when first Y1 and second Y1 are different

--- src/test/java/com/minibootcamp/pasti/LineTest.java
+++ src/test/java/com/minibootcamp/pasti/LineTest.java
@@ -40,4 +40,11 @@ public class LineTest {
         Line line2 = new Line(1, 2, 4, 4);
         assertFalse(line1.compareLine(line2));
     }
+
+    @Test
+    void compareLine_ShouldEqualFalse_WhenFirstY1andSecondY1Different() {
+        Line line1 = new Line(1, 2, 3, 4);
+        Line line2 = new Line(1, 1, 3, 4);
+        assertFalse(line1.compareLine(line2));
+    }
 }
